076a8d6361f614846b9dd0668ef24b1b34f0ed7a
scgarzonp <scgarzonp@gmail.com>
2024-01-02 10:28:30 -0500

    Feature:CSS Modules were implemented in Hero component, also SASS was installed after that

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import styles from "./Hero.module.css";
 
 export const Hero = () => {
+  console.log(styles);
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={styles.Hero}>
+      <h2>Hero Title</h2>
+      <h3>Subtitle</h3>
+    </section>
+  );
+};
