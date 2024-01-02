ecd14faaf6fa36ee89de66f0f02d2ea4f0fa1b73
scgarzonp <scgarzonp@gmail.com>
2024-01-02 08:40:44 -0500

    Feature:A folder of categories was created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log("hello world");
   return (
     <main className={styles.main}>
       <h1>Hello Everybody</h1>
