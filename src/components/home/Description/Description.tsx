4becc7f1c60393d408a341d9e7994ca92b4d8dbc
scgarzonp <scgarzonp@gmail.com>
2024-01-03 09:15:06 -0500

    Next image was implemented in description

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import styles from "./Description.module.sass";
+import Image from "next/image";
 
 export const Description = () => {
   return (
-    <section>
-      <img src="/images/description.png" alt="products marketplace" />
-      <h2>Description</h2>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
-        atque fuga eum quidem nam laborum eveniet doloribus nisi soluta modi
-        sapiente molestias consequuntur aliquid sequi omnis, voluptate magnam
-        itaque. Quia!
-      </p>
+    <section className={styles.Description}>
+      {/* Image carga imagenes con lazyloading por defecto */}
+      <Image
+        src="/images/description.png"
+        alt="products marketplace"
+        width={500} //siempre es necesario establecer un ancho con Image
+        height={300}
+        // priority={false} esto desactiva el lazyloading
+      />
+      <div className={styles.Description__text}>
+        <h2>Description</h2>
+        <p>
+          Future World: Your Gateway to Tomorrow's Tech! Dive into a world of
+          cutting-edge gadgets and gear. Stay ahead of the curve and redefine
+          your digital lifestyle with us.
+        </p>
+      </div>
     </section>
   );
 };
