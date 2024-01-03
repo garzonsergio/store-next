a445aae49652de0e59e7f4b39742693aad69520c
scgarzonp <scgarzonp@gmail.com>
2024-01-03 11:41:42 -0500

    optimization of fonts with next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
+import { Ubuntu } from "next/font/google";
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 import "app/sass/globals.sass";
 
-const inter = Inter({ subsets: ["latin"] });
+const ubuntu = Ubuntu({
+  weight: ["300", "500", "700"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -12,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={ubuntu.className}>
         <Header />
         {children}
         <Footer />
