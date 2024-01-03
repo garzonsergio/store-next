ce31a31126c4b04043115f93df3f31bc01b0699b
scgarzonp <scgarzonp@gmail.com>
2024-01-03 08:29:50 -0500

    Feature: Styles were added with SASS

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import styles from "./Hero.module.css";
+// import styles from "./Hero.module.css";
+import styles from "./Hero.module.sass";
 
 export const Hero = () => {
   console.log(styles);
   return (
     <section className={styles.Hero}>
-      <h2>Hero Title</h2>
-      <h3>Subtitle</h3>
+      <h1>Hero Title</h1>
+      <h2>Subtitle</h2>
     </section>
   );
 };
