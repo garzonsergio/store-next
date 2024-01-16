71c3659ed85b1d721debf4f8bf62b1cf95511f00
scgarzonp <scgarzonp@gmail.com>
2024-01-16 11:17:20 -0500

    Feature: A error component was created inside the grouping route

---
+++ src/app/(home)/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+  useEffect(() => {
+    console.log(error);
+  }, []);
+  return (
+    <div style={{ padding: "1rem" }}>
+      <h1>😞</h1>
+      <p>Ha ocurrido un error</p>
+      <button onClick={reset}>Intentar de nuevo</button>
+    </div>
+  );
+}
